fc74a6c3b0993c8775447c89f266160b97105a69
Tomasz Dudkowski <tomaszszd@gmail.com>
2020-05-23 19:36:35 +0200

    Add project files.

---
+++ ToolsLibrary/Helpers.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace ToolsLibrary
+{   
+    /// <summary>
+    /// Biblioteka Helpers
+    /// Autor: Tomasz Dudkowski
+    /// </summary>
+    public static class Helpers
+    {
+        /// <summary>
+        /// Kolorowanie składni terminala. Argument STRING
+        /// </summary>
+        /// <param name="consoleColor"><paramref name="consoleColor"/></param>
+        /// <param name="Text"><paramref name="Text"/></param>
+        public static void SyntaxColor(ConsoleColor consoleColor, string Text)
+        {
+            Console.ForegroundColor = consoleColor;
+            Console.WriteLine(Text);
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Kolorowanie składni terminala. Argument INT
+        /// </summary>
+        /// <param name="consoleColor"><paramref name="consoleColor"/></param>
+        /// <param name="Arg"><paramref name="Arg"/></param>
+        public static void SyntaxColor(ConsoleColor consoleColor, int Arg)
+        {
+            Console.ForegroundColor = consoleColor;
+            Console.WriteLine(Arg);
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Kolorowanie skłądni terminala. Argument FLOAT
+        /// </summary>
+        /// <param name="consoleColor"><paramref name="consoleColor"/></param>
+        /// <param name="Arg"><paramref name="Arg"/></param>
+        public static void SyntaxColor(ConsoleColor consoleColor, float Arg)
+        {
+            Console.ForegroundColor = consoleColor;
+            Console.WriteLine(Arg);
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Kolorowanie składni terminala. Argument DOUBLE
+        /// </summary>
+        /// <param name="consoleColor"><paramref name="consoleColor"/></param>
+        /// <param name="Arg"><paramref name="Arg"/></param>
+        public static void SyntaxColor(ConsoleColor consoleColor, double Arg)
+        {
+            Console.ForegroundColor = consoleColor;
+            Console.WriteLine(Arg);
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Konwertowanie STRING na Int32
+        /// </summary>
+        /// <param name="arg">arg</param>
+        /// <returns>Int32.Parse(arg)</returns>
+        public static dynamic Read(string arg)
+        {
+            return Int32.Parse(arg);
+        }
+
+        /// <summary>
+        /// Konwertowanie STRING na FLOAT
+        /// </summary>
+        /// <param name="arg">arg</param>
+        /// <returns>float.Parse(arg)</returns>
+        public static float ReadFloat(string arg)
+        {
+            return float.Parse(arg);
+        }
+
+        /// <summary>
+        /// Konwertowanie STRING na DOUBLE
+        /// </summary>
+        /// <param name="arg">arg</param>
+        /// <returns>double.Parse(arg)</returns>
+        public static double ReadDouble(string arg)
+        {
+            return double.Parse(arg);
+        }
+
+        /// <summary>
+        /// Kopiowanie dynamiczne
+        /// </summary>
+        /// <param name="toCopy">dynamic toCopy</param>
+        /// <returns>prettyCopy</returns>
+        public static dynamic DynamicCopy(dynamic toCopy)
+        {
+            var prettyCopy = toCopy;
+            return prettyCopy;
+        }
+    }
+}
---
+++ ToolsLibrary/PESELlib.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ToolsLibrary
+{
+    /// <summary>
+    /// Biblioteka PESEL
+    /// Autor: Tomasz Dudkowski
+    /// </summary>
+    public static class PESELlib
+    {
+        private static CultureInfo CulturePL = new System.Globalization.CultureInfo("pl-PL");
+
+        public static int ZwrocRokUrodzenia(dynamic a, dynamic b, dynamic c)
+        {
+            string rok = null;
+
+            if (c == 8 || c == 9)
+            {
+                rok = "18" + a + b;
+            }
+            else if (c == 0 || c == 1)
+            {
+                rok = "19" + a + b;
+            }
+            else if (c == 2 || c == 3)
+            {
+                rok = "20" + a + b;
+            }
+            else if (c == 4 || c == 5)
+            {
+                rok = "21" + a + b;
+            }
+            else if (c == 6 || c == 6)
+            {
+                rok = "22" + a + b;
+            }
+            int _intRok = Int32.Parse(rok);
+            return _intRok;
+        }
+
+        public static int ZwrocMiesiacUrodzenia(dynamic a, dynamic b)
+        {
+            if (a == 0 || a == 2 || a == 4 || a == 6 || a == 8)
+            {
+                a = 0;
+            }
+            else if (a == 9 || a == 3 || a == 5 || a == 7)
+            {
+                a = 1;
+            }
+
+            string _temp = a.ToString();
+            _temp = _temp + b.ToString();
+            int valueMiesiac = Int32.Parse(_temp);
+            return valueMiesiac;
+        }
+
+        public static DateTime ZwrocDzienUrodzenia(int rok, int miesiac, dynamic a, dynamic b)
+        {
+            string _temp = a.ToString();
+            _temp = _temp + b.ToString();
+            int dzien = Int32.Parse(_temp);
+            DateTime dateTime = new DateTime(rok, miesiac, dzien);
+            return dateTime;
+        }
+
+        public static string NazwaDniaTygodnia(DateTime dateTime)
+        {
+            string TextValue = CulturePL.DateTimeFormat.GetDayName(dateTime.DayOfWeek);
+            return TextValue;
+        }
+
+        public static string NazwaMiesiaca(int valueMiesiac)
+        {
+            string TextValue = CulturePL.DateTimeFormat.GetMonthName(valueMiesiac);
+            return TextValue;
+        }
+
+        public static string Plec(params dynamic[] args)
+        {
+
+            string _temp = "";
+            string plec;
+            foreach (dynamic item in args)
+            {
+                _temp = _temp + item.ToString();
+            }
+            int liczba = Int32.Parse(_temp);
+            if (liczba % 2 == 0)
+            {
+                plec = "Kobieta";
+            }
+            else
+            {
+                plec = "Mężczyzna";
+            }
+            return plec;
+        }
+
+        public static bool SumaKontrolna(int[] a)
+        {
+            // 1 3 7 9 1 3 7 9 1 3
+            int wynik = a[0] * 1 + a[1] * 3 + a[2] * 7 + a[3] * 9 + a[4] * 1 + a[5] * 3 + a[6] * 7 + a[7] * 9 + a[8] * 1 + a[9] * 3;
+
+            bool kontrola = false;
+            wynik = wynik % 10;
+            wynik = 10 - wynik;
+            wynik = wynik % 10;
+            if (wynik == a[10])
+            {
+                kontrola = true;
+            }
+            return kontrola;
+        }
+    }
+}
